b0430dd8406b53ed6bd487876dc090d1f3adf038
Dean <kirmmin.mmo@gmail.com>
2016-01-09 16:58:46 -0500

    Merge of files from dev

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    void OnTriggerEnter2D(Collider2D trigger) {
+        levelManager.LoadLevel("Win Screen");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
+    }
+}
